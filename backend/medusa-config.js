bd7f51da13c6ed39da556056eb0796926abe70e9
Phuong Hoang <65266367+phuonghoangcabin@users.noreply.github.com>
2024-10-10 17:14:16 +0700

    build vps

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -1,10 +1,11 @@
-import { loadEnv, Modules, defineConfig } from '@medusajs/utils';
+import { loadEnv, Modules, defineConfig } from "@medusajs/utils";
 
-const isDev = process.env.NODE_ENV === 'development';
+const isDev = process.env.NODE_ENV === "development";
 
 loadEnv(process.env.NODE_ENV, process.cwd());
 
-const backendUrl = process.env.RAILWAY_PUBLIC_DOMAIN_VALUE || 'http://localhost:9000';
+const backendUrl =
+  process.env.RAILWAY_PUBLIC_DOMAIN_VALUE || "http://localhost:9000";
 
 const plugins = [
   // 'medusa-fulfillment-manual'
@@ -12,89 +13,91 @@ const plugins = [
 
 const modules = {
   [Modules.AUTH]: {
-    resolve: '@medusajs/auth',
+    resolve: "@medusajs/auth",
     options: {
       providers: [
         {
-          resolve: '@medusajs/auth-emailpass',
-          id: 'emailpass',
-          options: {}
-        }
-      ]
-    }
+          resolve: "@medusajs/auth-emailpass",
+          id: "emailpass",
+          options: {},
+        },
+      ],
+    },
   },
   [Modules.FILE]: {
-    resolve: '@medusajs/file',
+    resolve: "@medusajs/file",
     options: {
       providers: [
         {
-          resolve: '@medusajs/file-local-next',
-          id: 'local',
+          resolve: "@medusajs/file-local-next",
+          id: "local",
           options: {
-            upload_dir: 'static',
-            backend_url: `${backendUrl}/static`
-          }
-        }
-      ]
-    }
+            upload_dir: "static",
+            backend_url: `${backendUrl}/static`,
+          },
+        },
+      ],
+    },
   },
 };
 
 // Redis configuration
 if (process.env.REDIS_URL) {
-  console.log('Redis url found, enabling event bus with redis');
+  console.log("Redis url found, enabling event bus with redis");
   modules[Modules.EVENT_BUS] = {
-    resolve: '@medusajs/event-bus-redis',
-    options: { 
-      redisUrl: process.env.REDIS_URL
-    }
+    resolve: "@medusajs/event-bus-redis",
+    options: {
+      redisUrl: process.env.REDIS_URL,
+    },
   };
 }
 
 // Stripe payment provider
-const stripeApiKey = process.env.STRIPE_API_KEY;
-const stripeWebhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
-const stripeConfigured = stripeApiKey && stripeWebhookSecret;
-if (stripeConfigured) {
-  console.log('Stripe api key and webhook secret found, enabling stripe payment provider');
-  modules[Modules.PAYMENT] = {
-    resolve: '@medusajs/payment',
-    options: {
-      providers: [
-        {
-          resolve: '@medusajs/payment-stripe',
-          id: 'stripe',
-          options: {
-            apiKey: stripeApiKey,
-            webhookSecret: stripeWebhookSecret
-          }
-        }
-      ]
-    }
-  };
-}
+// const stripeApiKey = process.env.STRIPE_API_KEY;
+// const stripeWebhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+// const stripeConfigured = stripeApiKey && stripeWebhookSecret;
+// if (stripeConfigured) {
+//   console.log('Stripe api key and webhook secret found, enabling stripe payment provider');
+//   modules[Modules.PAYMENT] = {
+//     resolve: '@medusajs/payment',
+//     options: {
+//       providers: [
+//         {
+//           resolve: '@medusajs/payment-stripe',
+//           id: 'stripe',
+//           options: {
+//             apiKey: stripeApiKey,
+//             webhookSecret: stripeWebhookSecret
+//           }
+//         }
+//       ]
+//     }
+//   };
+// }
 
 // SendGrid notification provider
 const sendgridApiKey = process.env.SENDGRID_API_KEY;
 const sendgridFrom = process.env.SENDGRID_FROM_EMAIL;
 const sendgridConfigured = sendgridApiKey && sendgridFrom;
 if (sendgridConfigured) {
-  console.log('SendGrid api key and from address found, enabling SendGrid notification provider');
+  console.log(
+    "SendGrid api key and from address found, enabling SendGrid notification provider"
+  );
   modules[Modules.NOTIFICATION] = {
-    resolve: '@medusajs/notification',
+    resolve: "@medusajs/notification",
     options: {
       providers: [
         {
-          resolve: '@medusajs/notification-sendgrid',
-          id: 'sendgrid',
+          resolve: "@medusajs/notification-sendgrid",
+          id: "sendgrid",
           options: {
-            channels: ['email'],
+            channels: ["email"],
             api_key: sendgridApiKey,
-            from: sendgridFrom
-          }
-        }
-      ]
-    }
+            from: sendgridFrom,
+          },
+        },
+      ],
+    },
   };
 }
 
@@ -105,11 +108,11 @@ const projectConfig = {
     authCors: process.env.AUTH_CORS,
     storeCors: process.env.STORE_CORS,
     jwtSecret: process.env.JWT_SECRET,
-    cookieSecret: process.env.COOKIE_SECRET
+    cookieSecret: process.env.COOKIE_SECRET,
   },
   database_url: process.env.DATABASE_URL,
-  database_type: 'postgres',
-  ...(process.env.REDIS_URL && { redisUrl: process.env.REDIS_URL })
+  database_type: "postgres",
+  ...(process.env.REDIS_URL && { redisUrl: process.env.REDIS_URL }),
 };
 
 const completeConfig = {
@@ -117,8 +120,8 @@ const completeConfig = {
   plugins,
   modules,
   admin: {
-    ...!isDev && { backendUrl }
-  }
+    ...(!isDev && { backendUrl }),
+  },
 };
 
 export default defineConfig(completeConfig);
